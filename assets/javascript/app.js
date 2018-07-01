e2c666dcd708f7e8c71e8f322005e4478c0e74ae
nohabroha <noahysimon@gmail.com>
2018-06-30 19:19:41 -0500

    basically working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,155 +20,138 @@ $(document).ready(function () {
 
 
     function generateGame() {
-        timer();
+        $(".question").show();
+        $(".choice1").show();
+        $(".choice2").show();
+        $(".choice3").show();
+        $(".choice4").show();
         $(".question").html("<img src="+questions[questionCounter]+">");
         $(".choice1").html(choice[questionCounter][0]);
         $(".choice2").html(choice[questionCounter][1]);
         $(".choice3").html(choice[questionCounter][2]);
         $(".choice4").html(choice[questionCounter][3]);
+        $(".endGame").hide();
+        $(".endGame2").hide();
     }
 
-
-    var correct = 0;
-    var incorrect = 0;
- 
-
-    function endGame() {
-        // correct, incorrect, 
+    function right() {
+        correct++;
         $(".question").hide();
         $(".choice1").hide();
         $(".choice2").hide();
         $(".choice3").hide();
         $(".choice4").hide();
+        $(".endGame").show();
+        $(".endGame2").show();
+        $(".endGame").html("<p>The correct answer is " + answer[questionCounter] + "</p>");
+        setTimeout(transitionTime, 1000 * 3);
+    }
+
+    function wrong() {
+        incorrect++;
+        $(".question").hide();
+        $(".choice1").hide();
+        $(".choice2").hide();
+        $(".choice3").hide();
+        $(".choice4").hide();
+        $(".endGame").show();
+        $(".endGame2").show();
+        $(".endGame").html("<p>The correct answer is " + answer[questionCounter] + "</p>");
+        setTimeout(transitionTime, 1000 * 3);
+    }
+
+    function endGame() {
+        clearInterval(clock);
+        $(".timer").hide();
+        $(".question").hide();
+        $(".choice1").hide();
+        $(".choice2").hide();
+        $(".choice3").hide();
+        $(".choice4").hide();
+        $(".endGame").show();
+        $(".endGame2").show();
         $(".endGame").html("<p>You got " + correct + "correct</p>");
         $(".endGame2").html("<p>You got " + incorrect + "incorrect</p>");
     }
 
 
+// starts game
+    if(questionCounter < 2) {
+        generateGame();
+        timer();
+    }
+// moves the game forward
+    function transitionTime() {
+        if (questionCounter < 1) {
+            questionCounter++;
+            generateGame();
+            counter = 5;
+            timer();
+        }
+        else {
+            endGame();
+        }
+    }
 
-
+    var correct = 0;
+    var incorrect = 0;
+    
+//listens for a click and checks the answer
     $(".choice1").click(function() {
         if (choice[questionCounter][0] === answer[questionCounter]) {
             console.log("correct");
-            correct++;
-            questionCounter++;
-            if (questionCounter <= victoryCondition) {
-                generateGame();
-                counter = 5;
-            }
-            if (questionCounter === victoryCondition + 1) {
-                endGame();
-            }
+            right();
         } else {
             console.log("incorrect");
-            incorrect++;
-            questionCounter++;
-            if (questionCounter <= victoryCondition) {
-                generateGame();
-                counter = 5;
-            }
-            if (questionCounter === victoryCondition + 1) {
-                endGame();
-            }
+            
+            wrong();
         }
     })
     $(".choice2").click(function() {
         if (choice[questionCounter][1] === answer[questionCounter]) {
             console.log("correct");
-            correct++;
-            questionCounter++;
-            if (questionCounter <= victoryCondition) {
-                generateGame();
-                counter = 5;
-            }
-            if (questionCounter === victoryCondition + 1) {
-                endGame();
-            }
+            right();
         } else {
             console.log("incorrect");
-            incorrect++;
-            questionCounter++;
-            if (questionCounter <= victoryCondition) {
-                generateGame();
-                counter = 5;
-            }
-            if (questionCounter === victoryCondition + 1) {
-                endGame();
-            }
+            wrong();
         }
     })
     $(".choice3").click(function() {
         if (choice[questionCounter][2] === answer[questionCounter]) {
             console.log("correct");
-            correct++;
-            questionCounter++;
-            if (questionCounter <= victoryCondition) {
-                counter = 3;
-                generateGame();
-                counter = 5;
-            }
-            if (questionCounter === victoryCondition + 1) {
-                endGame();
-            }
+            right();
         } else {
             console.log("incorrect");
-            incorrect++;
-            questionCounter++;
-            if (questionCounter <= victoryCondition) {
-                generateGame();
-                counter = 5;
-            }
-            if (questionCounter === victoryCondition + 1) {
-                endGame();
-            }
+            wrong();
         }
     })
     $(".choice4").click(function() {
         if (choice[questionCounter][3] === answer[questionCounter]) {
             console.log("correct");
-            correct++;
-            questionCounter++;
-            if (questionCounter <= victoryCondition) {
-                generateGame();
-            }
-            if (questionCounter === victoryCondition + 1) {
-                endGame();
-            }
+            right();
         } else {
             console.log("incorrect");
-            incorrect++;
-            questionCounter++;
-            if (questionCounter <= victoryCondition) {
-                generateGame();
-            }
-            if (questionCounter === victoryCondition + 1) {
-                endGame();
-            }
+            wrong();
         }
     })
+    
 
-
-
+// countdown timer used on every question
     var counter = 5;
     var clock;
-    var victoryCondition = 1;
     function timer() {
         clock = setInterval(decrement, 1000);
         function decrement() {
             if (counter > 0) {
                 counter--;
             }
-            if (counter === 0) {
+            if (counter <= 0) {
                 clearInterval(clock);
-                console.log(victoryCondition);
                 console.log(questionCounter);
-                if (questionCounter >= victoryCondition) {
+                if(questionCounter < 1) {
+                    wrong();
+                } else {
                     endGame();
-                }
-                if(questionCounter < victoryCondition) {
-                    questionCounter++;
-                    generateGame();
-                    counter = 5;
                 }
             }
             $(".timer").html(counter);
@@ -176,9 +159,7 @@ $(document).ready(function () {
     }
 
 
-    if(questionCounter < 10) {
-        generateGame();
-    }
+   
 
 
 
